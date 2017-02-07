c3991a7205ff41083400002bdf282ae54259cbce
lennseg <lennseg@gmail.com>
2017-02-07 13:39:59 -0500

    changed function on js

--- week2/js/main.js
+++ week2/js/main.js
@@ -7,3 +7,21 @@ document.getElementById("mojitoName").style.fontSize = "11pt";
 document.getElementById("inputEmail").style.fontSize = "11pt";
 
 document.getElementById("inputPassword").style.fontSize = "11pt";
+
+function mDown(obj) {
+    obj.style.backgroundColor = "turquoise";
+    obj.innerHTML = "Release Me";
+}
+
+function mUp(obj) {
+    obj.style.backgroundColor = "red";
+    obj.innerHTML = "Thank You";
+}
+
+function mOver(obj) {
+    obj.innerHTML = "Are You Sure?";
+}
+
+function mOut(obj) {
+    obj.innerHTML = "DONE!";
+}
