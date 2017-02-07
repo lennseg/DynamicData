028f4e84b1262630249936d2fa051e58812c274a
lennseg <lennseg@gmail.com>
2017-02-07 13:22:50 -0500

    changed js

---
+++ week2/js/main.js
@@ -0,0 +1,9 @@
+document.getElementById("rum").style.fontSize = "12pt";
+
+document.getElementById("fruit").style.fontSize = "12pt";
+
+document.getElementById("mojitoName").style.fontSize = "12pt";
+
+document.getElementById("inputEmail").style.fontSize = "12pt";
+
+document.getElementById("inputPassword").style.fontSize = "12pt";
