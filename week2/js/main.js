e0a62c015ff22d6665c9d3849971d32802b0638a
lennseg <lennseg@gmail.com>
2017-02-07 13:24:04 -0500

    font size change through js

--- week2/js/main.js
+++ week2/js/main.js
@@ -1,9 +1,9 @@
-document.getElementById("rum").style.fontSize = "12pt";
+document.getElementById("rum").style.fontSize = "11pt";
 
-document.getElementById("fruit").style.fontSize = "12pt";
+document.getElementById("fruit").style.fontSize = "11pt";
 
-document.getElementById("mojitoName").style.fontSize = "12pt";
+document.getElementById("mojitoName").style.fontSize = "11pt";
 
-document.getElementById("inputEmail").style.fontSize = "12pt";
+document.getElementById("inputEmail").style.fontSize = "11pt";
 
-document.getElementById("inputPassword").style.fontSize = "12pt";
+document.getElementById("inputPassword").style.fontSize = "11pt";
