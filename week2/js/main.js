968e0d3a9b9e4c7cba50d0125845949b870f8cfc
lennseg <lennseg@gmail.com>
2017-02-07 13:45:16 -0500

    added mouse functions

--- week2/js/main.js
+++ week2/js/main.js
@@ -8,20 +8,15 @@ document.getElementById("inputEmail").style.fontSize = "11pt";
 
 document.getElementById("inputPassword").style.fontSize = "11pt";
 
-function mDown(obj) {
-    obj.style.backgroundColor = "turquoise";
-    obj.innerHTML = "Release Me";
-}
-
-function mUp(obj) {
-    obj.style.backgroundColor = "red";
-    obj.innerHTML = "Thank You";
-}
-
 function mOver(obj) {
     obj.innerHTML = "Are You Sure?";
 }
 
 function mOut(obj) {
     obj.innerHTML = "DONE!";
+}
+
+function mClick (obj) {
+    obj.innerHTML = "Start Mixing!";
+    
 }
