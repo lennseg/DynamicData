1de606d90a024cbdfbd1ba732c1207bcdd907424
lennseg <lennseg@gmail.com>
2017-02-15 15:13:49 -0500

    class examples continued

--- week4/story/views/helpers/basic.js
+++ week4/story/views/helpers/basic.js
@@ -2,7 +2,14 @@ const Handlebars = require('handlebars');
 
 Handlebars.registerHelper('bold', function(options){
     
-    var test= new Handlebars.Safestrings('<b>'+ options.fn(this) +'</b>');
+    //var test= new Handlebars.Safestrings('<b>'+ options.fn(this) +'</b>');
+    
+    
+});
+
+Handlebars.registerHelper('list', function(options){
+    
+    //var test= new Handlebars.Safestrings('<b>'+ options.fn(this) +'</b>');
     
     
 });
