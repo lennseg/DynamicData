7866db94c36ad5110a1bcf0c904a76c4d71cbc39
lennseg <lennseg@gmail.com>
2017-02-21 00:49:02 -0500

    creating mojito form from class example

--- week4/story/app.js
+++ week4/story/app.js
@@ -7,6 +7,11 @@ const Inert = require('inert');
 const Path = require('path');
 const Handlebars = require('handlebars');
 
+const fs = require("fs");
+
+const Sequelize = require('sequelize');
+
+
 const server = new Hapi.Server({
     connections: {
         routes: {
@@ -21,6 +26,38 @@ server.connection({
     port: 3000
 });
 
+var sequelize = new Sequelize('db', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    },
+
+    // SQLite only
+    storage: 'db.sqlite'
+});
+
+
+var Monster = sequelize.define('monster', {
+
+    monsterName: {
+        type: Sequelize.STRING
+    },
+    quarters: {
+        type: Sequelize.STRING
+    },
+    weapon: {
+        type: Sequelize.STRING
+    },
+    message: {
+        type: Sequelize.STRING
+    }
+
+});
+
 server.register([Blipp, Inert, Vision], () => {});
 
 server.views({
@@ -30,10 +67,9 @@ server.views({
     path: 'views',
     layoutPath: 'views/layout',
     layout: 'layout',
-    helpersPath: 'views/helpers'
-        //partialsPath: 'views/partials'
+    helpersPath: 'views/helpers',
+    //partialsPath: 'views/partials'
 });
-
 
 
 server.route({
@@ -41,19 +77,9 @@ server.route({
     path: '/',
     handler: {
         view: {
-            template: 'index',
-            context: {
-                title: 'story',
-                message: 'On a Spring Morning, Phil the Dino went to the park. Walking, he was startled by a ....',
-                //pic: '/images/'
-                menu: [
-                    {item: "squirrel"},
-                    {item: "kite"},
-                    {item: "computer"}
-                    ]
-                }
-            }
+            template: 'index'
         }
+    }
 });
 
 server.route({
@@ -62,18 +88,156 @@ server.route({
     handler: {
         directory: {
             path: './',
-            listing: true,
-            index: false,
-            redirectToSlash: true
+            listing: false,
+            index: false
         }
     }
 });
 
-    server.start((err) => {
+server.route({
+    method: 'POST',
+    path: '/form',
+    handler: function (request, reply) {
+        console.log(request.payload.firstname);
+        var firstname = request.payload.firstname;
 
-        if (err) {
-            throw err;
+        reply.view('formresponse', {
+            firstname: firstname,
+        })
+    }
+
+});
+
+server.route({
+    method: 'GET',
+    path: '/savefile',
+    handler: {
+        view: {
+            template: 'savefile'
         }
-        console.log(`Server running at: ${server.info.uri}`);
+    }
 
-    });
+});
+
+server.route({
+    method: 'POST',
+    path: '/savefile',
+    handler: function (request, reply) {
+        var formResponse = request.payload;
+        //console.log(formResponse);
+        var jsonForm = JSON.stringify(formResponse);
+        //console.log(typeof(jsonForm));
+        console.log("Writing Data");
+        fs.writeFile('savedata.txt', jsonForm, function (err) {
+            if (err) {
+                return console.error(err);
+            }
+
+            console.log("reading saved data");
+            fs.readFile('savedata.txt', function (err, data) {
+                if (err) {
+                    return console.error(err);
+                }
+
+                console.log(data.toString());
+
+            });
+        });
+        reply("File Saved");
+    }
+});
+
+server.route({
+
+    method: 'GET',
+    path: '/savedata',
+    handler: function (request, reply) {
+        console.log("Let's read newly written data");
+        var currentData = "";
+        fs.readFile('savedata.txt', function (err, data) {
+            if (err) {
+                return console.error(err);
+            }
+            currentData = JSON.parse(data.toString());
+            console.log("Asynchronous read: " + currentData);
+
+            reply.view('savedata', {
+                formresponse: currentData,
+            });
+        });
+
+    }
+});
+
+
+
+server.route({
+    method: 'GET',
+    path: '/createDB',
+    handler: function (request, reply) {
+        // force: true will drop the table if it already exists
+        Monster.sync({
+            force: true
+        })
+        reply("Database Created")
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/destroyAll',
+    handler: function (request, reply) {
+
+        Monster.drop();
+
+        reply("destroy all");
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/addDB/{monsterName}/{quarters}/{weapon}/{message}',
+    handler: function(request, reply){
+        Monster.create({
+            monsterName: encodeURIComponent(request.params.monsterName),
+            quarters: encodeURIComponent(request.params.quarters),
+            weapon: encodeURIComponent(request.params.weapon),
+            message: encodeURIComponent(request.params.message),
+        });
+
+        Monster.sync();
+
+        reply("saved entry");
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/displayAll',
+    handler: function (request, reply) {
+
+        Monster.findAll().then(function(users){
+
+            //console.log(users[0].monsterName);
+
+            var allUsers = JSON.stringify(users);
+
+            reply.view('dbresponse', {
+                dbresponse: allUsers
+            });
+        })
+    }
+});
+
+
+
+
+
+server.start((err) => {
+
+    if (err) {
+        throw err;
+    }
+    console.log(`Server running at: ${server.info.uri}`);
+
+});
--- week5/basicForm/app.js
+++ week5/basicForm/app.js
@@ -7,6 +7,11 @@ const Inert = require('inert');
 const Path = require('path');
 const Handlebars = require('handlebars');
 
+const fs = require("fs");
+
+const Sequelize = require('sequelize');
+
+
 const server = new Hapi.Server({
     connections: {
         routes: {
@@ -21,6 +26,38 @@ server.connection({
     port: 3000
 });
 
+var sequelize = new Sequelize('db', 'username', 'password', {
+    host: 'localhost',
+    dialect: 'sqlite',
+
+    pool: {
+        max: 5,
+        min: 0,
+        idle: 10000
+    },
+
+    // SQLite only
+    storage: 'db.sqlite'
+});
+
+
+var Monster = sequelize.define('monster', {
+
+    monsterName: {
+        type: Sequelize.STRING
+    },
+    quarters: {
+        type: Sequelize.STRING
+    },
+    weapon: {
+        type: Sequelize.STRING
+    },
+    message: {
+        type: Sequelize.STRING
+    }
+
+});
+
 server.register([Blipp, Inert, Vision], () => {});
 
 server.views({
@@ -30,7 +67,7 @@ server.views({
     path: 'views',
     layoutPath: 'views/layout',
     layout: 'layout',
-    //helpersPath: 'views/helpers',
+    helpersPath: 'views/helpers',
     //partialsPath: 'views/partials'
 });
 
@@ -58,19 +95,143 @@ server.route({
 });
 
 server.route({
-
-
     method: 'POST',
     path: '/form',
     handler: function (request, reply) {
         console.log(request.payload.firstname);
-        var firstname = encodeURIComponent(request.payload.firstname);
+        var firstname = request.payload.firstname;
+
         reply.view('formresponse', {
             firstname: firstname,
-        });
+        })
     }
 
 });
+
+server.route({
+    method: 'GET',
+    path: '/savefile',
+    handler: {
+        view: {
+            template: 'savefile'
+        }
+    }
+
+});
+
+server.route({
+    method: 'POST',
+    path: '/savefile',
+    handler: function (request, reply) {
+        var formResponse = request.payload;
+        //console.log(formResponse);
+        var jsonForm = JSON.stringify(formResponse);
+        //console.log(typeof(jsonForm));
+        console.log("Writing Data");
+        fs.writeFile('savedata.txt', jsonForm, function (err) {
+            if (err) {
+                return console.error(err);
+            }
+
+            console.log("reading saved data");
+            fs.readFile('savedata.txt', function (err, data) {
+                if (err) {
+                    return console.error(err);
+                }
+
+                console.log(data.toString());
+
+            });
+        });
+        reply("File Saved");
+    }
+});
+
+server.route({
+
+    method: 'GET',
+    path: '/savedata',
+    handler: function (request, reply) {
+        console.log("Let's read newly written data");
+        var currentData = "";
+        fs.readFile('savedata.txt', function (err, data) {
+            if (err) {
+                return console.error(err);
+            }
+            currentData = JSON.parse(data.toString());
+            console.log("Asynchronous read: " + currentData);
+
+            reply.view('savedata', {
+                formresponse: currentData,
+            });
+        });
+
+    }
+});
+
+
+
+server.route({
+    method: 'GET',
+    path: '/createDB',
+    handler: function (request, reply) {
+        // force: true will drop the table if it already exists
+        Monster.sync({
+            force: true
+        })
+        reply("Database Created")
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/destroyAll',
+    handler: function (request, reply) {
+
+        Monster.drop();
+
+        reply("destroy all");
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/addDB/{monsterName}/{quarters}/{weapon}/{message}',
+    handler: function(request, reply){
+        Monster.create({
+            monsterName: encodeURIComponent(request.params.monsterName),
+            quarters: encodeURIComponent(request.params.quarters),
+            weapon: encodeURIComponent(request.params.weapon),
+            message: encodeURIComponent(request.params.message),
+        });
+
+        Monster.sync();
+
+        reply("saved entry");
+    }
+});
+
+server.route({
+    method: 'GET',
+    path: '/displayAll',
+    handler: function (request, reply) {
+
+        Monster.findAll().then(function(users){
+
+            //console.log(users[0].monsterName);
+
+            var allUsers = JSON.stringify(users);
+
+            reply.view('dbresponse', {
+                dbresponse: allUsers
+            });
+        })
+    }
+});
+
+
+
+
 
 server.start((err) => {
 
