8df550c817fe5bdd3c203ed8c0a035a51b1dd6d9
lennseg <lennseg@gmail.com>
2017-03-21 00:12:12 -0400

    chart.js code

--- midterm/speech-form/public/sketch.js
+++ midterm/speech-form/public/sketch.js
@@ -1,19 +1,44 @@
 var txt;
 var counts = {};
+var keys = [];
 
 function preload(){
     txt = loadStrings('mlk-speech.txt');
 }
 
 function setup(){
+        noCanvas();
+    
     var allwords = txt.join("\n");
     var tokens = allwords.split(/\W+/);
-    console.log(tokens);
+    //console.log(tokens);
     
-    for(var i = 0; i < tokens.length; i++){
-        var word = tokens[i];
+    for(var i = 0; i < tokens.length; i++) {
+        var word = tokens[i].toLowerCase();
+        if(!/\d+/.test(word)) {
+            //console.log(word);
+        
+        if (counts[word] === undefined) {       
+        counts[word] = 1;
+            keys.push(word);
+        } else {
+                counts[word] = counts[word] + 1;
+            }
+        }
+    }
+    
+    keys.sort(compare);
+    
+    function compare (a,b){
+        var countA = counts[a];
+        var countB = counts[b];
+        
+        return countB - countA;
+    }
+    for(var i = 0; i < keys.length; i++){
+        var key = keys[i];
+        createDiv(key + " " + counts[key]);
         
     }
     
-    noCanvas();
 }
