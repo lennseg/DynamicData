a3d52c7c9a05143e3d70411833834f3427776e08
lennseg <lennseg@gmail.com>
2017-04-04 12:57:26 -0400

    recovering files from branch errors

---
+++ midterm/speech/public/js/main.js
@@ -0,0 +1,30 @@
+const CHART = document.getElementById("barChart");
+let barChart = new Chart(CHART, {
+    type: 'bar'
+    , data: {
+        labels: ["Freedom", "We", "Negro", "Let", "Dream", "Ring"]
+        , datasets: [
+            {
+                label: "MLK Speech Word Frequencies"
+                , backgroundColor: [
+                'rgba(255, 99, 132, 0.2)'
+                , 'rgba(54, 162, 235, 0.2)'
+                , 'rgba(255, 206, 86, 0.2)'
+                , 'rgba(75, 192, 192, 0.2)'
+                , 'rgba(153, 102, 255, 0.2)'
+                , 'rgba(255, 159, 64, 0.2)'
+            ]
+                , borderColor: [
+                'rgba(255,99,132,1)'
+                , 'rgba(54, 162, 235, 1)'
+                , 'rgba(255, 206, 86, 1)'
+                , 'rgba(75, 192, 192, 1)'
+                , 'rgba(153, 102, 255, 1)'
+                , 'rgba(255, 159, 64, 1)'
+            ]
+                , borderWidth: 1
+                , data: [20, 30, 13, 13, 11, 12]
+        , }
+    ]
+    }
+});
